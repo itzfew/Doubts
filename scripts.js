cb169f6e29680c71e7e37b5491d98beb2c545074
Itzfew <113174780+itzfew@users.noreply.github.com>
2024-09-06 18:16:03 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,15 +3,16 @@ import { getFirestore, collection, addDoc, getDocs, orderBy, query, serverTimest
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-analytics.js";
 
-// Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDLKPOqok8VS3gR4TAEGCEH4IEJL8kKpvw",
-    authDomain: "ind-edu-f63b0.firebaseapp.com",
-    projectId: "ind-edu-f63b0",
-    storageBucket: "ind-edu-f63b0.appspot.com",
-    messagingSenderId: "405906160405",
-    appId: "1:405906160405:web:7040d4f0118fa01d13071c",
-    measurementId: "G-EPQM943Y2V"
+  apiKey: "AIzaSyAJVzWcSVu7nW-069bext5W6Nizx4sfxIA",
+  authDomain: "edu-hub-c81b5.firebaseapp.com",
+  databaseURL: "https://edu-hub-c81b5-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "edu-hub-c81b5",
+  storageBucket: "edu-hub-c81b5.appspot.com",
+  messagingSenderId: "560742513136",
+  appId: "1:560742513136:web:102edd272982704fdb8535",
+  measurementId: "G-78TC8XTPF7"
 };
 
 // Initialize Firebase
